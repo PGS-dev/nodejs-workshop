412713c5cf8601ab9b38c5c0efedbff920ec020a
Łukasz Kochajewski <lkochajewski@degree.no>
2018-11-09 13:35:30 +0100

    use fat arrow function syntax

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,10 +5,10 @@ const multer = require('multer')
 
 // disk storage strategy
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: (req, file, cb) => {
         cb(null, './uploads/')
     },
-    filename: function(req, file, cb) {
+    filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`)
     }
 })
